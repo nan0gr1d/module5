e889a0459cb6303ab8f26c5159e99ce71db11849
Urban <prestofan@gmail.com>
2024-07-18 16:17:58 +0300

    Методы __new__  & __del__ добавлены,

--- module_5_3.py
+++ module_5_3.py
@@ -2,10 +2,11 @@
 module_5_3
 """
 class House():
+    houses_history = []
+
     def __init__(self, name, number_of_floors):
         self.name = name
         self.number_of_floors = number_of_floors
-
 
     def go_to(self, new_floor):
         if new_floor < 1 or new_floor > self.number_of_floors:
@@ -19,7 +20,6 @@ class House():
         должен возвращать кол-во этажей здания self.number_of_floors.
         """
         return self.number_of_floors
-
 
     def __str__(self):
         """
@@ -91,7 +91,28 @@ class House():
     def __iadd__(self, value):
         return self.__add__(value)
 
+    def __del__(self):
+        print(f"{self.name} снесён, но он останется в истории")
 
+    def __new__(cls, *args, **kwargs):
+        cls.houses_history.append(args[0])
+        return super().__new__(cls)
+
+
+h1 = House('ЖК Эльбрус', 10)
+print(House.houses_history)
+h2 = House('ЖК Акация', 20)
+print(House.houses_history)
+h3 = House('ЖК Матрёшки', 20)
+print(House.houses_history)
+
+# Удаление объектов
+del h2
+del h3
+
+print(House.houses_history)
+
+"""
 h1 = House('домик', 2)
 h2 = House('Домина', 20)
 print(h1)
@@ -127,5 +148,6 @@ print(h1 >= h2) # __ge__
 print(h1 < h2) # __lt__
 print(h1 <= h2) # __le__
 print(h1 != h2) # __ne__
+"""
 
 #eof-module_5_3
